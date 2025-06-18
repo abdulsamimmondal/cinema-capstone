55f2fe1812a69b808cea7c7219023a5fc0bed906
ABDUL SAMIM MONDAL <mercuryman912@gmail.com>
2025-06-18 15:10:21 +0530

    Update SearchUser.java

--- src/SearchUser.java
+++ src/SearchUser.java
@@ -1,12 +1,9 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,9 +17,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SearchUser")
 public class SearchUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
- 
+
+    // Define constants to read database connection details from environment variables
+    private static final String DB_HOST = System.getenv("DB_HOST");
+    private static final String DB_PORT = System.getenv("DB_PORT");
+    private static final String DB_NAME = System.getenv("DB_NAME");
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
+ 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();   //PrintWriter variable, out initialize.
+		PrintWriter out = response.getWriter();   //PrintWriter variable, out initialize.
 		out.print("<!DOCTYPE html><html><head><link href=\"//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css\" rel=\"stylesheet\" id=\"bootstrap-css\">\r\n" +
 				"<script src=\"//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js\"></script>\r\n" +
 				"<script src=\"//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\r\n" +
@@ -36,18 +40,18 @@ public class SearchUser extends HttpServlet {
 		
 		String role = request.getParameter("role");
 		
-		if(role.equals("admins")){
-			try
-			{  //A try statement.
-				Class.forName("org.postgresql.Driver");
-				Connection con=DriverManager
-		                .getConnection("jdbc:postgresql://localhost:5432/cinema","postgres","12345"); //Connection with the postgresql database with following credentials.
+        // Construct the JDBC URL using the environment variables (moved outside the if/else for reusability)
+        String jdbcUrl = "jdbc:postgresql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
 
+		if(role.equals("admins")){
+			try (Connection con = DriverManager.getConnection(jdbcUrl, DB_USER, DB_PASSWORD)) // Auto-closable connection
+			{  //A try statement.
+				Class.forName("org.postgresql.Driver");
 				
 				PreparedStatement ps1=con.prepareStatement("SELECT id,username,fullname,createdby_admin FROM admins where username=?;");
 				ps1.setString(1,request.getParameter("username"));
 				ResultSet rs = ps1.executeQuery();
-				while(rs.next())  //A while statement.
+				while(rs.next())  //A while statement.
 				 {
 					out.print("<tr><td style=\"border: solid 1px #DDEEEE;color: #333;padding: 10px; text-shadow: 1px 1px 1px #fff;\">");
 					out.println(rs.getInt(1));
@@ -64,6 +68,7 @@ public class SearchUser extends HttpServlet {
 					//Prints the Availiable Movies' Table.
 				}
 				rs.close();
+                ps1.close(); // Close ps1
 			}
 			catch(Exception e)
 			{ //Catch Statement.
@@ -72,17 +77,14 @@ public class SearchUser extends HttpServlet {
 			out.print("</table></body></html>"); //Close the html components.
 		}
 		else if(role.equals("contentadmins")) {
-			try
-			{  //A try statement.
+			try (Connection con = DriverManager.getConnection(jdbcUrl, DB_USER, DB_PASSWORD)) // Auto-closable connection
+			{  //A try statement.
 				Class.forName("org.postgresql.Driver");
-				Connection con=DriverManager
-		                .getConnection("jdbc:postgresql://localhost:5432/cinema","postgres","12345"); //Connection with the postgresql database with following credentials.
-
 				
 				PreparedStatement ps1=con.prepareStatement("SELECT id,username,fullname,createdby_admin FROM contentadmins where username=?;");
 				ps1.setString(1,request.getParameter("username"));
 				ResultSet rs = ps1.executeQuery();
-				while(rs.next())  //A while statement.
+				while(rs.next())  //A while statement.
 				 {
 					out.print("<tr><td style=\"border: solid 1px #DDEEEE;color: #333;padding: 10px; text-shadow: 1px 1px 1px #fff;\">");
 					out.println(rs.getInt(1));
@@ -99,6 +101,7 @@ public class SearchUser extends HttpServlet {
 					//Prints the Availiable Movies' Table.
 				}
 				rs.close();
+                ps1.close(); // Close ps1
 			}
 			catch(Exception e)
 			{ //Catch Statement.
@@ -106,18 +109,15 @@ public class SearchUser extends HttpServlet {
 			}
 			out.print("</table></body></html>"); //Close the html components.
 		}
-		else {
-			try
-			{  //A try statement.
+		else { // Handles 'clients' role
+			try (Connection con = DriverManager.getConnection(jdbcUrl, DB_USER, DB_PASSWORD)) // Auto-closable connection
+			{  //A try statement.
 				Class.forName("org.postgresql.Driver");
-				Connection con=DriverManager
-		                .getConnection("jdbc:postgresql://localhost:5432/cinema","postgres","12345"); //Connection with the postgresql database with following credentials.
-
 				
 				PreparedStatement ps1=con.prepareStatement("SELECT id,username,fullname FROM clients where username=?;");
 				ps1.setString(1,request.getParameter("username"));
 				ResultSet rs = ps1.executeQuery();
-				while(rs.next())  //A while statement.
+				while(rs.next())  //A while statement.
 				 {
 					out.print("<tr><td style=\"border: solid 1px #DDEEEE;color: #333;padding: 10px; text-shadow: 1px 1px 1px #fff;\">");
 					out.println(rs.getInt(1));
@@ -131,6 +131,7 @@ public class SearchUser extends HttpServlet {
 					//Prints the Availiable Movies' Table.
 				}
 				rs.close();
+                ps1.close(); // Close ps1
 			}
 			catch(Exception e)
 			{ //Catch Statement.
